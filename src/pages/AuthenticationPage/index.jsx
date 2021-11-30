090ad49d7c4c61c173a775a26379ce6efdca2dff
FrankPZT <lihanyang19940724@gmail.com>
2021-12-01 05:26:24 +1000

    added lint rules a sample page

---
+++ src/pages/AuthenticationPage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const AuthenticationPage = () => (
+        <div className={styles.container}>
+            <h1>This is the Authenticatoin page!</h1>
+        </div>
+    )
+
+export default AuthenticationPage;
